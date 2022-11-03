58e14079f5a87311eb80791882fca297fec767ab
wotmlaile <117249414+wotmlaile@users.noreply.github.com>
2022-11-03 17:02:33 +0800

    Add files via upload

---
+++ Git_print.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8-*-
+# @time     : 2022/11/3  16:58
+# @Author   : liz
+
+for i in range(1,10):
+    print(" ")
+    for j in range(1,i+1):
+        print("{0}*{1}={2}".format(j,i,j*i),end=" ")
+
+
